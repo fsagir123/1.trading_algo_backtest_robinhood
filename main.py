c49828817f39a656fed14180d4e34bb3ff87127c
Fasil Sagir <fsagir@hntb.com>
2024-10-03 14:17:57 -0500

    Your commit message for the changes in main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import RF_Algo_Backtest_07172024
-import VWAP_Algo_Backtest_07152024
+import RF_Algo_Backtest
+import RF_Algo_Live
+import VWAP_Algo_Backtest
+import VWAP_Algo_Live_Manually_Execute
 import login
 import pyfiglet
-import pandas as pd
 
 #call the login module to log into Robinhood
 login.main()
@@ -12,45 +13,34 @@ stock_ticker = ["TSLA","AAPL"]
 
 #Define the time periods of interest
 # options - 5minute”, “10minute”, “hour”, “day”
-aggregation_window = ["day"]
+time_period = ["day","hour"]
 
 #Define the time period shorter than the time period of interest
 # options - 5minute”, “10minute”, “hour”, “day”
-shorter_aggregation_window = ["hour"]
-#full_data_span
-full_data_span = ["5year"]
+shorter_time_period = ["5minute","5minute"]
+#span
+span = ["year","3month"]
 
 def printing_method_name(text):
     text = pyfiglet.figlet_format(text)
     return text
 
-def save_results_to_excel(results, filename):
-    # Create a DataFrame from the results
-    results_df = pd.DataFrame(results, columns=['Stock Ticker', 'Algorithm', 'Initial Balance', 'Final Balance', 'Cumulative Returns (%)'])
     
-    # Save to Excel
-    results_df.to_excel(filename, index=False)
-    print(f"Results saved to {filename}")    
 
 if __name__ == '__main__':
-
-  results = []
     
   for i in range(len(stock_ticker)):
-      for j in range(len(aggregation_window)):
+      for j in range(len(time_period)):
 
-        print(printing_method_name("ML Backtest for "+stock_ticker[i]+" and interval "+aggregation_window[j]))    
-        method ="ML"
-        Ensemble_results, LSTM_results = RF_Algo_Backtest_07172024.main(stock_ticker[i],aggregation_window[j],shorter_aggregation_window[j],method,full_data_span[j])
-        results.append(Ensemble_results)
-        results.append(LSTM_results)
-
-        method ="Algo"
-        print(printing_method_name("VWAP  backtest result for "+stock_ticker[i]+" and interval "+aggregation_window[j]))
-        VWAP_results = VWAP_Algo_Backtest_07152024.main( stock_ticker[i],aggregation_window[j],shorter_aggregation_window[j],method,full_data_span[j])
-        results.append(VWAP_results)
-        
-        
-  # Save all results to an Excel file
-  save_results_to_excel(results, 'backtest_results.xlsx')
+        print(printing_method_name("Random Forest Backtest for "+stock_ticker[i]+" and interval "+time_period[j]))    
+        method ="Random Forest"
+        RF_Algo_Backtest.main(stock_ticker[i],time_period[j],method,span[j])
+        print(printing_method_name("Random Forest trading decision for "+stock_ticker[i]+" and interval "+time_period[j]))
+        RF_Algo_Live.main(stock_ticker[i],time_period[j],method,span[j],shorter_time_period[j])
+        method ="VWAP"
+        print(printing_method_name("VWAP  backtest result for "+stock_ticker[i]+" and interval "+time_period[j]))
+        VWAP_Algo_Backtest.main( stock_ticker[i],time_period[j],method,span[j])
+        print(printing_method_name("VWAP trading decision for "+stock_ticker[i]+" and interval "+time_period[j]))
+        VWAP_Algo_Live_Manually_Execute.main( stock_ticker[i],time_period[j],method,span[j])
+    
 
